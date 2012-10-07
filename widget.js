8b5fc072c3442eff57e22ce30df0653415d414b3
Sam Parkinson <sparkinson@outlook.com>
2012-10-07 10:45:45 +0100

    Several improvments
    Removed a useless for loop.
    Stoped it becoming a global var.
    Added to window.isitup for developer usage.
    Added referal param to json request (so I can see where the widgets are
    getting used).

--- widget.js
+++ widget.js
@@ -1,38 +1,32 @@
 /*
-* Is it up? Widget
-* ================
-* 
-* Version: 1.0 beta
-* Author: Sam Parkinson (@r3morse)
-* jsFiddle: http://jsfiddle.net/sparkinson/tspPU/
-* GitHub: https://github.com/r3morse/isitup-widget
-*
-* To-Do:
-* - make custom getelementsbyclassname()
-*
-*/
-(function() {
-    isitup = {
+ * Is it up? Widget
+ * ================
+ *
+ *   Author: Sam Parkinson (@r3morse)
+ * jsFiddle: http://jsfiddle.net/sparkinson/tspPU/
+ *   GitHub: https://github.com/r3morse/isitup-widget
+ *
+ */
+ (function (doc) {
+    var isitup = {
         // Our api host
         server: "http://isitup.org/",
-
-        // Node list of our widgets
-        nodes: document.getElementsByClassName("isitup-widget"),
 
         // The main function:
         // - Parses each widget parameters
         // - Inserts the empty widget html
         // - Makes the api requests
-        exec: function() {
+        exec: function ()
+        {
             // Shorten the most used variables
-            var doc = document,
-                node = this.nodes,
+            var node = document.getElementsByClassName("isitup-widget"),
                 server = this.server,
                 requested = [];
 
             var i, len;
-            for (i = 0, len = node.length; i < len; i++) {
 
+            for (i = 0, len = node.length; i < len; i++)
+            {
                 // Initalise our html vaiable
                 var HTML = "";
 
@@ -40,9 +34,11 @@
                 var domain = node[i].getAttribute("data-domain");
 
                 // No option-domain set...
-                if (!domain) {
+                if (!domain)
+                {
                     // So set it to the hostname
                     domain = window.location.hostname;
+
                     node[i].setAttribute("data-domain", domain);
                 }
 
@@ -60,34 +56,45 @@
                 node[i].innerHTML = HTML;
 
                 // Check the domain is valid
-                if (this.is_domain(domain)) {
+                if (this.is_domain(domain))
+                {
                     // And the json hasn't already been requested
-                    if (!this.in_list(domain, requested)) {
+                    if (!this.in_list(domain, requested))
+                    {
                         // Insert our JSON request into the <head>
                         this.get_json(domain);
+
                         requested.push(domain);
                     }
-                    // If the domain is invalid
-                } else {
+                }
+                else // If the domain is invalid
+                {
                     // Run update() with an invalid domain response locally
-                    this.update({
+                    this.update(
+                    {
                         "domain": domain,
                         "status_code": 3
                     });
                 }
-
             }
+
+            return true;
         },
 
         // Function to inject our jsonp into the <head>
         // @input domain (str)        domain of the site to be checked
-        get_json: function(domain) {
+        get_json: function (domain)
+        {
             var t = "script",
                 d = document;
+
             var j = d.createElement(t),
                 p = d.getElementsByTagName(t)[0];
+
             j.type = "text/javascript";
-            j.src = this.server + domain + ".json?callback=isitup.update";
+
+            j.src = this.server + domain + ".json?callback=isitup.update&ref=" + window.location.hostname;
+
             p.parentNode.insertBefore(j, p);
         },
 
@@ -95,60 +102,62 @@
         // - Finds the widget to update
         // - Updates the widgets image & link
         // @input result (json)       JSON object from the api response
-        update: function(result) {
+        update: function (result)
+        {
             // Update widget with the latest widget nodes
-            var node = document.getElementsByClassName("isitup-widget"),
-                widget = [];
+            var node = document.getElementsByClassName("isitup-widget");
 
             // Go through the widgets and find the one we're updating
             var i, len;
-            for (i = 0, len = node.length; i < len; i++) {
-                if (node[i].getAttribute("data-domain") == result.domain && !node[i].getAttribute("data-checked")) {
-                    // Select the widget
-                    widget.push(node[i]);
-                    // Update it with the checked parameter
-                    node[i].setAttribute("data-checked", true);
-                }
-            }
 
-            // Loop incase a domain is used in more than one widget
-            var j;
-            for (j = 0, len = widget.length; j < len; j++) {
-
-                // Look at the status code from the response
-                switch (result.status_code) {
+            for (i = 0, len = node.length; i < len; i++)
+            {
+                if (node[i].getAttribute("data-domain") == result.domain && !node[i].getAttribute("data-checked"))
+                {
+                    // Look at the status code from the response
+                    switch (result.status_code)
+                    {
                     // If the site is online
-                case 1:
-                    // Change the icon to green
-                    this.set_image("online", widget[j]);
+                    case 1:
+                        // Change the icon to green
+                        this.set_image("online", node[i]);
 
-                    // If an uplink has been set
-                    if (widget[j].hasAttribute("data-uplink")) {
-                        // Change the link to the user defined uplink
-                        this.set_link(widget[j].getAttribute("data-uplink"), widget[j]);
-                    }
-                    break;
+                        // If an uplink has been set
+                        if (node[i].hasAttribute("data-uplink"))
+                        {
+                            // Change the link to the user defined uplink
+                            this.set_link(node[i].getAttribute("data-uplink"), node[i]);
+                        }
+
+                        break;
 
                     // If it's offline
-                case 2:
-                    // Change the icon to red
-                    this.set_image("offline", widget[j]);
+                    case 2:
+                        // Change the icon to red
+                        this.set_image("offline", node[i]);
 
-                    // If a downlink has been set
-                    if (widget[j].hasAttribute("data-downlink")) {
-                        // Change the link to the user defined downlink
-                        this.set_link(widget[j].getAttribute("data-downlink"), widget[j]);
-                    }
-                    break;
+                        // If a downlink has been set
+                        if (node[i].hasAttribute("data-downlink"))
+                        {
+                            // Change the link to the user defined downlink
+                            this.set_link(node[i].getAttribute("data-downlink"), node[i]);
+                        }
+
+                        break;
 
                     // If the domain is invalid
-                case 3:
-                    // Set the image to yellow
-                    this.set_image("error", widget[j]);
+                    case 3:
+                        // Set the image to yellow
+                        this.set_image("error", node[i]);
 
-                    // Set the link to http://isitup.org/d/<data-domain>
-                    this.set_link(this.server + "d/" + widget[j].getAttribute("data-domain"), widget[j]);
-                    break;
+                        // Set the link to http://isitup.org/d/<data-domain>
+                        this.set_link(this.server + "d/" + node[i].getAttribute("data-domain"), node[i]);
+
+                        break;
+                    }
+
+                    // Update it with the checked parameter
+                    node[i].setAttribute("data-checked", true);
                 }
             }
         },
@@ -156,22 +165,30 @@
         // Function to set the src parameter of a given <img> tag
         // @input image (str)        name of the image to insert
         // @input node                <img> node to insert the image into
-        set_image: function(image, node) {
-            node.getElementsByClassName("isitup-icon")[0].firstChild.setAttribute("src", this.server + "widget/img/" + image + ".png");
+        set_image: function (image, node)
+        {
+            node.getElementsByClassName("isitup-icon")[0]
+                .firstChild
+                .setAttribute("src", this.server + "widget/img/" + image + ".png");
         },
 
         // Function to set the href parameter of a given <a> tag
         // @input link (str)        url to insert
         // @input node                <a> node to insert the url into
-        set_link: function(link, node) {
-            node.getElementsByClassName("isitup-domain")[0].firstChild.setAttribute("href", link);
+        set_link: function (link, node)
+        {
+            node.getElementsByClassName("isitup-domain")[0]
+                .firstChild
+                .setAttribute("href", link);
         },
 
         // A simple regex test for a domain
         // @input domain (str)        domain to test
         // @output boolean            true if domain is valid, otherwise false
-        is_domain: function(domain) {
+        is_domain: function (domain)
+        {
             re = /^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/;
+
             return (re.test(domain)) ? true : false;
         },
 
@@ -179,17 +196,25 @@
         // @input value                value to check for
         // @input list                list to go through
         // @output boolean
-        in_list: function(value, list) {
+        in_list: function (value, list)
+        {
             var i, len;
-            for (i = 0, len = list.length; i < len; i++) {
-                if (list[i] == value) {
+
+            for (i = 0, len = list.length; i < len; i++)
+            {
+                if (list[i] == value)
+                {
                     return true;
                 }
             }
+
             return false;
         }
     };
 
     // Run the widget.
-    window.addEventListener('load', function() { isitup.exec(); }, false);
-}());
+    window.addEventListener('load', function () { isitup.exec(); }, false);
+
+    // Add the object as a window child so it can be used if required.
+    window.isitup = isitup;
+}(document));
